5dd1aa278423f221bec6b355f988b4d36bb46696
Yerkanat Salaly <erko.salaly@gmail.com>
2024-10-30 10:14:20 +0100

    addedHeading

--- fahrenheit-to-celcius.c
+++ fahrenheit-to-celcius.c
@@ -1,17 +1,20 @@
-// print Fahrenheit - celsius table for fahr = 0, 20, 40, ..., 300
+// Print Fahrenheit - Celsius table for fahr = 0, 20, 40, ..., 300
 
 #include <stdio.h>
 
 int main () {
   int lower, upper, step;
   float fahr, celsius;
-  lower = 0;
-  upper = 300;
-  step = 20;
+  lower = 0;    // lower bound of temperature table
+  upper = 300;  // upper bound
+  step = 20;    // Increment of fahr in each step of the while loop
   fahr = lower;
+  printf("Fahrenheit - Celsius\n");
   while (fahr <= upper) {
     celsius = (5.0 / 9.0) * (fahr - 32.0);
-    printf("%4.0f %6.1f\n", fahr, celsius);
+    printf("%10.0f - %5.1f\n", fahr, celsius); // %4.0f: 4 is the minimum field width, 0 specifies zero digits after the decimal point.
+    // Each % sign corresponds to a variable in the printf argument list:
+    // For %4.0f, fahr is used because it's the first variable after the format string.
     fahr = fahr + step;
   }
 }
