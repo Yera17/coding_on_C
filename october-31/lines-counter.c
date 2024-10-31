74f0a488c7e0f0f2cb902a173daeba6385f7d60d
Yerkanat Salaly <erko.salaly@gmail.com>
2024-10-31 08:13:36 +0100

    structured-and-line-counter-added

---
+++ october-31/lines-counter.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main () { // counts each line and prints at the end
+  int nc, c;
+
+  while ((c = getchar()) != EOF) {  // while loop is asigning c with input and
+                                    // if c is not EOF, it keeps looping
+    if (c == '\n')  // checks if new line is entered
+      ++nc;         // if so, nc gets incremented by 1
+  }
+
+   // when loop stops the code below outputs the result grammatically correct
+
+  if (nc == 0)
+    return printf("No line was entered");
+  else if (nc == 1)
+    return printf("%d line was entered", nc);
+
+  return printf("%d lines were entered", nc);
+}
